ae34f0a53fdb7f22614635ae9bbfb912ba301f4c
3388ShubinPavel <mopcshow@gmail.com>
2024-11-20 23:07:43 +0300

    fix code style

--- hawk-java/src/main/java/org/catcher/CustomUncaughtExceptionHandler.java
+++ hawk-java/src/main/java/org/catcher/CustomUncaughtExceptionHandler.java
@@ -6,19 +6,19 @@ package org.catcher;
 public class CustomUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
     private final Thread.UncaughtExceptionHandler defaultHandler;
 
-  /**
-   * Initializes the custom handler and stores the default handler.
-   */
+    /**
+     * Initializes the custom handler and stores the default handler.
+     */
     public CustomUncaughtExceptionHandler() {
         this.defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
     }
 
-  /**
-   * Handles uncaught exceptions by logging them and invoking the default handler.
-   *
-   * @param t the thread that threw the exception
-   * @param e the thrown exception
-   */
+    /**
+     * Handles uncaught exceptions by logging them and invoking the default handler.
+     *
+     * @param t the thread that threw the exception
+     * @param e the thrown exception
+     */
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         System.out.printf("Exception in thread %s: %s\n", t.getName(), e.getMessage());
